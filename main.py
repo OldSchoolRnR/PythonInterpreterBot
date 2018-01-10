a75ba831b67f934fca1dd4601c49ad38fb46e8d6
OldSchoolRnR <monster9kill@i.ua>
2018-01-10 22:17:45 +0200

    Added consistent output, removed bot token

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import subprocess, timeout_decorator
+import subprocess
+import timeout_decorator
 from timeout_decorator import TimeoutError
 import os
-
-
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import bot_token
 
-BOT_TOKEN = "499748172:AAF-GpIxh7qojA93_XBMKC0PD2sWtpy2Pus"
+stop_all=False
+
+BOT_TOKEN = bot_token.BOT_TOKEN
 bot = telegram.Bot(token=BOT_TOKEN)
 updater = Updater(token=BOT_TOKEN)
 dispatcher = updater.dispatcher
@@ -15,31 +17,72 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 
 def start(bot, update):
-    bot.send_message(chat_id = update.message.chat_id, text="I'm a Python bot, please write python code to me!")
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a Python bot, please write python code to me!")
 
 
-@timeout_decorator.timeout(30, use_signals=False, exception_message="Time out!",)
-def run_code(f, update):
-        process = subprocess.Popen(['python', os.getcwd() + '/' + f.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out = process.communicate()
-        bot.send_message(chat_id=update.message.chat_id, text=out[0] + out[1])
+def proc(f, update, real_time):
+    for path in run_code(f, real_time, update):
+        pass
+        # bot.send_message(chat_id=update.message.chat_id, text=path.encode("utf-8"))
 
 
 def echo(bot, update):
-    f = open(update.message.from_user.name,"w+")
-    f.write(update.message.text)
+    global stop_all
+    f = open(update.message.from_user.name, "w+")
+
+    #Check for os and real time
+
+    temp = update.message.text.encode('utf-8')
+    if (temp.startswith("REAL_TIME_PROGRAM")):
+        temp = temp[17:]
+        real_time = True
+    else:
+        real_time = False
+
+    f.write(temp)
     f.flush()
     f.close()
-    temp = update.message.text.encode('utf-8')
-    check_str = "os."
-    if (temp.find(check_str) == -1):
+    if (temp.find("os.") == -1):
         try:
-            run_code(f, update)
+            run_code(f, real_time, update)
         except TimeoutError:
+            stop_all = True
             bot.send_message(chat_id=update.message.chat_id, text="Your program worked too long!")
     else:
-        bot.send_message(chat_id=update.message.chat_id, text="Sorry, you used forbidden command '" + check_str + "'")
+        bot.send_message(chat_id=update.message.chat_id, text="Sorry, you used forbidden command '" + "os." + "'")
     os.remove(os.getcwd() + '/' + f.name)
+    stop_all = False
+
+
+@timeout_decorator.timeout(5, use_signals=False)
+def run_code(f, real_time, update):
+    process = subprocess.Popen(['python', os.getcwd() + '/' + f.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # kill_proc = lambda p: process.kill()
+    # timer = Timer(5, kill_proc, [process])
+    # timer = Timer(5, process.kill)
+    # timer.start()
+    if real_time:
+        while not stop_all:
+            line = process.stdout.readline().rstrip()
+            if not line and process.stderr:
+                line = process.stderr.readline().rstrip()
+
+            if not line:
+                break
+            # yield line
+            bot.send_message(chat_id=update.message.chat_id, text=line.encode("utf-8"))
+    else:
+        stdout, stderr = process.communicate()
+        # yield stdout + stderr
+        bot.send_message(chat_id=update.message.chat_id, text=stdout.encode("utf-8")+stderr.encode("utf-8"))
+    #if timer.is_alive():
+    #    timer.cancel()
+
+
+def stop(bot, update):
+    global stop_all
+    stop_all = True
+    bot.send_message(chat_id=update.message.chat_id, text="You stopped your program.")
 
 
 def unknown(bot, update):
@@ -49,9 +92,11 @@ def unknown(bot, update):
 echo_handler = MessageHandler(Filters.text, echo)
 unknown_handler = MessageHandler(Filters.command, unknown)
 start_handler = CommandHandler('start', start)
+stop_handler = CommandHandler('stop', stop)
 
 
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(stop_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(unknown_handler)
 
