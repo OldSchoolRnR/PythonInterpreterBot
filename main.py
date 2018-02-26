9227a4922243f1189c50bb72c0fd608efab5197e
OldSchoolRnR <monster9kill@i.ua>
2018-02-26 20:34:41 +0200

    Added check mode button_2.

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def start(bot, update):
 
 
 def help(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="This bot have 2 modes. Work on real time mode and without it. For work in real time mode type \"REAL_TIME_PROGRAM\" (without brackets) and your code from a new line. If you don't need this - just type your code.")
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="This bot have 2 modes. Work on real time mode and without it. For work in real time mode type \"REAL_TIME_PROGRAM\" (without brackets) and your code from a new line. If you don't need this - just type your code.")
 
 
 def echo(bot, update):
@@ -99,7 +100,6 @@ def run_code(f, real_time, update):
         stdout, stderr = process.communicate()
 
         bot.send_message(chat_id=update.message.chat_id, text=stdout.encode("utf-8")+stderr.encode("utf-8"))
-
 
 
 def stop(bot, update):
